a0033b28a1930b21b1cb5e606c977a42a551b246
barbibrussa <barbibrussa@gmail.com>
2021-06-25 21:17:16 -0300

    Use Pokemon component to list pokemons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <v-row wrap>
+      <v-col
+        cols="3"
+        :key="index"
+        v-for="(pokemon, index) in list"
+      >
+        <pokemon :pokemon="pokemon">
+        </pokemon>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Pokemon from '@/components/Pokemon.vue';
+import client from '@/api/client';
 
 export default Vue.extend({
   name: 'Home',
+  components: {
+    Pokemon,
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  beforeMount() {
+    client.get('pokemon?limit=151').then((res: any) => {
+      this.list = res.data.results;
+      console.log(this.list);
+    });
+  },
 });
 </script>
